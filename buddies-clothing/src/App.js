d59cffa6d4856dfa6d4f4fe901f80a9d952b207f
sanjay <sanjayy7890@gmail.com>
2022-08-01 15:18:24 -0400

    added Sass and updated categories

--- buddies-clothing/src/App.js
+++ buddies-clothing/src/App.js
@@ -1,34 +1,36 @@
 
+import './categories.styles.scss';
+
 const App = () => {
 
   const categories = [
     {
       id:1,
-      title: 'Men',
-    },
-    {
-      id:2,
-      title: 'Women',
-    },
-    {
-      id:3,
       title: 'Caps',
     },
     {
-      id:4,
+      id:2,
       title: 'Jackets',
     },
     {
-      id:5,
+      id:3,
       title: 'Sneakers',
+    },    
+    {
+      id:4,
+      title: 'Men',
+    },
+    {
+      id:5,
+      title: 'Women',
     }
   ]
 
   return (
         <div className="categories-div">{/*container for categories*/}
-          {categories.map(({title})=>(
-            <div className="category-div">{/*container for each category*/}
-            {/*<img/> placeholder for image that is to be added later  */ }
+          {categories.map(({title, id})=>(
+            <div key={id} className="category-div">{/*container for each category*/}
+            <div className='background-image'/> {/*<img/> placeholder for image that is to be added later  */ }
             <div className="category-body-div">
               <h2>{title}</h2>
               <p>Explore</p>
