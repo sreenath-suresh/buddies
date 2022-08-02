9a92a7c346f608a30b1ddf1428f4101f2ef1ce15
sanjay <sanjayy7890@gmail.com>
2022-08-01 20:00:35 -0400

    created new component Categories and updated styling and images

--- buddies-clothing/src/App.js
+++ buddies-clothing/src/App.js
@@ -1,44 +1,45 @@
 
+import CategoryItem from './components/category-item/category-item.component';
 import './categories.styles.scss';
 
 const App = () => {
 
   const categories = [
     {
-      id:1,
-      title: 'Caps',
+      "id": 1,
+      "title": "Caps",
+      "imageUrl": "https://i.ibb.co/fnHbWSV/cap.jpg"
     },
     {
-      id:2,
-      title: 'Jackets',
+      "id": 2,
+      "title": "Jackets",
+      "imageUrl": "https://i.ibb.co/kqj0xg0/jacket.jpg"
     },
     {
-      id:3,
-      title: 'Sneakers',
-    },    
-    {
-      id:4,
-      title: 'Men',
+      "id": 3,
+      "title": "Sneakers",
+      "imageUrl": "https://i.ibb.co/YBhPwPL/sneakers-2.jpg"
     },
     {
-      id:5,
-      title: 'Women',
-    }
-  ]
+      "id": 4,
+      "title": "Men",
+      "imageUrl": "https://i.ibb.co/zb34bm8/men.jpg"
 
-  return (
-        <div className="categories-div">{/*container for categories*/}
-          {categories.map(({title, id})=>(
-            <div key={id} className="category-div">{/*container for each category*/}
-            <div className='background-image'/> {/*<img/> placeholder for image that is to be added later  */ }
-            <div className="category-body-div">
-              <h2>{title}</h2>
-              <p>Explore</p>
-            </div> 
-          </div>
-          ))}
-        </div>   
+    },
+    {
+      "id": 5,
+      "title": "Women",
+      "imageUrl": "https://i.ibb.co/4JkGxWk/women.jpg"
+    }  ]
+
+    return (
+      <div className='categories-div'>
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
     );
-};
-
-export default App;
+  };
+  
+  export default App;
+  
