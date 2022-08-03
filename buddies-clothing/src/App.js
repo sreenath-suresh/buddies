af91fc283e465288f72c4e5e7b597c2c200f7701
sanjay <sanjayy7890@gmail.com>
2022-08-03 10:14:42 -0400

    built home component, navigation component, shop component and implemented routing between the components using outlets

--- buddies-clothing/src/App.js
+++ buddies-clothing/src/App.js
@@ -1,40 +1,32 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
+
+
+
+const Shop = () => {
+  return(
+    <div>
+      <div>
+        <h1>Placeholder for Shop</h1>
+      </div>
+    </div>
+  )
+}
+
 
 
 const App = () => {
 
-  const categories = [
-    {
-      "id": 1,
-      "title": "Caps",
-      "imageUrl": "https://i.ibb.co/fnHbWSV/cap.jpg"
-    },
-    {
-      "id": 2,
-      "title": "Jackets",
-      "imageUrl": "https://i.ibb.co/kqj0xg0/jacket.jpg"
-    },
-    {
-      "id": 3,
-      "title": "Sneakers",
-      "imageUrl": "https://i.ibb.co/YBhPwPL/sneakers-2.jpg"
-    },
-    {
-      "id": 4,
-      "title": "Men",
-      "imageUrl": "https://i.ibb.co/zb34bm8/men.jpg"
-
-    },
-    {
-      "id": 5,
-      "title": "Women",
-      "imageUrl": "https://i.ibb.co/4JkGxWk/women.jpg"
-    }  ]
-
     return (
-      <Directory categories={categories} />
+     <Routes>
+      <Route path='/' element={<Navigation/>}>
+        <Route index element = {<Home/>} />
+        <Route path = 'shop' element = {<Shop/>} />
 
-    );
+      </Route>
+     </Routes> 
+    )
   };
   
   export default App;
