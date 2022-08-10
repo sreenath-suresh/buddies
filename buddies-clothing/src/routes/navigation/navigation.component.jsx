08eae707d721d6ddf5962559c641fa29cde03f5d
sanjay <sanjayy7890@gmail.com>
2022-08-10 19:13:27 -0400

    implemented react context for state management as well as re-rendering from context

--- buddies-clothing/src/routes/navigation/navigation.component.jsx
+++ buddies-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,14 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { ReactComponent as BuddiesLogo } from '../../assets/logo.svg';
+
+import { UserContext } from '../../contexts/user.context';
+
 import './navigation.styles.scss';
 
 const Navigation = () => {
+    const { currentUser } = useContext(UserContext);
+//    console.log(currentUser);
     return (
       <Fragment>
         <div className='navigation'>
@@ -14,9 +19,15 @@ const Navigation = () => {
             <Link className='nav-link' to='/shop'>
               SHOP
             </Link>
-            <Link className='nav-link' to='/auth'>
-              SIGN IN
-            </Link>
+            {
+              currentUser ? (
+                  <span className='nav-link'>SIGN OUT</span>
+                ) : (
+                    <Link className='nav-link' to='/auth'>
+                      SIGN IN
+                    </Link>
+                  )
+            }
           </div>
         </div>
         <Outlet />
