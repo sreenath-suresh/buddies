879845b747b127353d91d2c57f46d895e5e58c99
sanjay <sanjayy7890@gmail.com>
2022-08-15 23:52:57 -0400

    created hash map for storing data retrieved from firestore and updated shop component

--- buddies-clothing/src/contexts/categories.context.jsx
+++ buddies-clothing/src/contexts/categories.context.jsx
@@ -3,19 +3,19 @@ import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.jsx'
 //import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.jsx';
 //import SHOP_DATA from '../shop-data.js';
 
-export const ProductsContext = createContext({
-  products: [],
+export const CategoriesContext = createContext({
+  categoriesMap: {},
 });
 
-export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
 
 
  useEffect(() =>{
       const getCategoriesMap = async () => {
         const categoryMap = await getCategoriesAndDocuments(); 
-        console.log("Hello");
         console.log(categoryMap); 
+        setCategoriesMap(categoryMap);
       };
       getCategoriesMap();
  },[]);
@@ -26,10 +26,10 @@ export const ProductsProvider = ({ children }) => {
   //   addCollectionAndDocuments('categories', SHOP_DATA);
   // }, []);
 
-  const value = { products };
+  const value = { categoriesMap };
   return (
-    <ProductsContext.Provider value={value}>
+    <CategoriesContext.Provider value={value}>
       {children}
-    </ProductsContext.Provider>
+    </CategoriesContext.Provider>
   );
 };
